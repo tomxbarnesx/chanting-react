9d3a0401ca26763418471ff44af599eacfff5af1
Tom Barnes <barnes.thomas.c@gmail.com>
2019-02-07 15:24:54 -0500

    Updates

---
+++ precache-manifest.ff92f114dbaa575f82dac5d8d67dffdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0fb4cb8f9cdfa9a4fd4",
+    "url": "/chanting-react/static/js/runtime~main.b0fb4cb8.js"
+  },
+  {
+    "revision": "75a837d8609ce7c9a09a",
+    "url": "/chanting-react/static/js/main.75a837d8.chunk.js"
+  },
+  {
+    "revision": "633607e3d63de35bf266",
+    "url": "/chanting-react/static/js/1.633607e3.chunk.js"
+  },
+  {
+    "revision": "75a837d8609ce7c9a09a",
+    "url": "/chanting-react/static/css/main.774619a9.chunk.css"
+  },
+  {
+    "revision": "508364ff386934fd00cf53c68ac18c78",
+    "url": "/chanting-react/index.html"
+  }
+];
